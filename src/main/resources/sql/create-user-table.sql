7f0c08f6b99701d5b102156b8df62384481b12b9
Rajesh Kanojia <rajesh.kanojia@navitas.com>
2022-06-24 16:34:58 +0530

    Error handling for POST-Users

---
+++ src/main/resources/sql/create-user-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name  VARCHAR(100) NOT NULL,
+    last_name  VARCHAR(100) DEFAULT NULL,
+    email  VARCHAR(100) NOT NULL,
+    date_of_birth DATE,
+    date_of_joining DATE,
+    password     VARCHAR(255) NOT NULL,
+    account_status ENUM('active', 'inactive', 'block', 'suspended') DEFAULT 'inactive',
+    email_verification_status   ENUM('verified', 'unverified') DEFAULT 'unverified',
+    PRIMARY KEY ( id ),
+    UNIQUE KEY (email)
+);
